d15a00cd79442cbda8e1459a28350766693c3312
Waqas Hamid <waqashamid722@gmail.com>
2018-03-26 02:14:36 +0530

    Added whitenoise to serve static content

--- test_proj/settings.py
+++ test_proj/settings.py
@@ -25,8 +25,7 @@ SECRET_KEY = '&zw!we5rke5)uxumpkgwxhodos8klg4sw)6%%$p)kd=5szvyw3'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '127.0.0.1', ]
 
 # Application definition
 
