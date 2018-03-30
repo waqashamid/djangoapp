dcd2d08eb614142b911cb45072050bf8423f49e5
Waqas Hamid <waqashamid722@gmail.com>
2018-03-30 18:05:39 +0530

    Added DRF Docs

--- api/views.py
+++ api/views.py
@@ -1,10 +1,6 @@
-from django.shortcuts import render
-
-# Create your views here.
-
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 
-@api_view(['GET', 'POST'])
+@api_view(['GET'])
 def gettingstarted(request):
     return Response(data={"message" : "Hello BKL!"})
--- test_proj/urls.py
+++ test_proj/urls.py
@@ -17,7 +17,7 @@ from django.conf.urls import url, include
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^', include('rest_framework_docs.urls')),
+    url(r'^docs', include('rest_framework_docs.urls')),
     url(r'^admin/', admin.site.urls),
     url(r'api', include('api.urls')),
 ]
